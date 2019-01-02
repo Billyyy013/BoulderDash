1ffb33f3ae950d81f967f44e57d6f6aa009cbadf
Koen B <khcbaude@avans.nl>
2019-01-02 15:08:14 +0100

    t

--- BoulderDashApp/BoulderDashApp/Model/Boulder.cs
+++ BoulderDashApp/BoulderDashApp/Model/Boulder.cs
@@ -31,14 +31,21 @@ namespace BoulderDashApp.Model
         // al de move methodes die los in de entity klassen staan moeten nog overerven van 1 move klasse. die moet dus nog gemaakt worden
         public override void Move(Tile tile, Tile next)
         {
+            //Als hij dus naar beneden kan vallen dan doet hij dat en returned hij true anders false 
             if (!next.PlaceEntity(this, null))
             {
+                //Als er een entity onder een boulder ofzo ligt dan kan een boulder ook schuin vallen anders niet
                 if (this.Tile.Below.Entity != null)
                 {
+                    //dit is linksom vallen
+                    //Als er geen entity links van de boulder en links onder van de boulder zit kan de boulder vallen
                     if (this.Tile.Left.Entity == null && this.Tile.Left.Below.Entity == null)
                     {
+                        //Hier word de boulder eerst naar links gezet als dit lukt kan de boulder dus worden verplaatst naar links onder
+                        //(als dit niet lukt betekent dit dat er bevoorbeeld een mud tile zit ofzo)
                         if (this.Tile.Left.PlaceEntity(this, null))
                         {
+                            //hier word de boulder naar links onder verplaatst als dit niet lukt dan moet de verplaatsing die hierboven is gedaan terug gedraaid worden
                             if (!this.Tile.Below.PlaceEntity(this, null))
                             {
                                 this.Tile.Right.PlaceEntity(this, null);
@@ -46,6 +53,8 @@ namespace BoulderDashApp.Model
                         }
 
                     }
+                    //dit is rechtsom vallen
+                    //hetzelfde als hierboven
                     else if (this.Tile.Right.Entity == null && this.Tile.Right.Below.Entity == null)
                     {
                         if (this.Tile.Right.PlaceEntity(this, null))
