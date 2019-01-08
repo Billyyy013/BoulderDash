8e3ffd8fd585d64ce5116cd1daf534f277249cb4
Koen B <khcbaude@avans.nl>
2019-01-08 17:38:58 +0100

    Rubble creation

---
+++ BoulderDashApp/BoulderDashApp/Model/Rubble.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoulderDashApp.Model
+{
+    public class Rubble : Entity
+    {
+        public override void Collision(Entity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
